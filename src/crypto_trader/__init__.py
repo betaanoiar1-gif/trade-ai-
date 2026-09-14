a864097cdba4a7d7f6e6eb7959272a9e52042fd2
betaanoiar1-gif <betaanoiar1@gmail.com>
2026-09-14 17:13:32 +0100

    style: fix package import formatting

--- src/crypto_trader/__init__.py
+++ src/crypto_trader/__init__.py
@@ -1,5 +1,6 @@
 """Crypto AI Trader package."""
-from .trader import CryptoTrader
-from .models import TradePlan, RiskLimits, Position
 
-__all__=["CryptoTrader","TradePlan","RiskLimits","Position"]
+from .models import Position, RiskLimits, TradePlan
+from .trader import CryptoTrader
+
+__all__ = ["CryptoTrader", "Position", "RiskLimits", "TradePlan"]
